1f8a4010b516085aa91d1000d70dd5215a77cb44
daniliuskosmaches <nasarspektrreas@gmail.com>
2026-02-01 20:36:19 +0500

    added an code execution method for loading java bytecode in memory for jvm that will run it

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,3 @@
-package org.example;
 import javax.tools.JavaCompiler;
 import javax.tools.ToolProvider;
 import java.io.File;
@@ -6,17 +5,31 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 
-import static jdk.vm.ci.hotspot.HotSpotJVMCIRuntime.Option.Compiler;
 
 
 public class Main {
+    private String className;
+    public Main(String className) {
+        this.className = className;
+    }
+
     public void CompileCode(String className,String code) throws IOException {
         File file = new File(className + ".java");
-        FileWriter writer = new FileWriter(file);
-        writer.write(code);
-        writer.flush();
+
+       try( FileWriter writer = new FileWriter(file)) {
+           writer.write(code);
+           writer.flush();
+       }
         JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-        String result = compiler.run(null, null, null, file.getPath());
+       if (compiler == null) {
+           throw new RuntimeException("No Java compiler available");
+
+       }
+        int result = compiler.run(null, null, null, file.getPath());
+       if (result != 0) {
+           throw new RuntimeException("Compilation failed");
+
+       }
 
 
 
@@ -27,11 +40,14 @@ public class Main {
 
 
     }
-    public void ExecuteCode() throws Exception {
-        File ByteCodeFile = new File(result);
-
-
-        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+    public void ExecuteCode(String className) throws Exception {
+        File folder = new File("./");
+        URL url = folder.toURI().toURL();
+        URLClassLoader classLoader = new URLClassLoader(new URL[]{url});
+        Class<?> clazz = classLoader.loadClass(className);
+        Object obj = clazz.getDeclaredConstructor().newInstance();
+        Method method = clazz.getMethod("main");
+        method.invoke(obj);
 
 
 
